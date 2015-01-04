99d202c0952f74113a7dc0d289a6743c78b12f85
Adrienne Johnson <adriennetjohnson@gmail.com>
2015-01-03 20:43:46 -0500

    second commit

--- rubyhomework8.rb
+++ rubyhomework8.rb
@@ -1,13 +1,13 @@
-def count_words(string)     
+def count_words(text)     
 frequency = Hash.new(0) 
-string.scan(/[-'\w]+/) {|word| frequency[word.downcase] += 1 } 
+text.scan(/[-'\w]+/) {|word| frequency[word.downcase] += 1 } 
 frequency.each { |k, v| puts "'#{k.upcase}' is the word used most in this text." if v ==
 frequency.values.max }  
 end
 
-string = "A day can really slip by when you're deliberately avoiding what you're supposed to do."
+text = "A day can really slip by when you're deliberately avoiding what you're supposed to do."
 
-count_words(string)
+count_words(text)
 
 
 
